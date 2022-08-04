e9c40fc86b975f2ea2cfc7c58e21266ed40a454c
DIPAYAN BANIK <88474401+dipayanjishu@users.noreply.github.com>
2022-08-04 21:42:59 +0530

    before timer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,14 @@ import "./app.css";
 import { FaDollarSign } from "react-icons/fa";
 import { useState } from "react";
 import Trivia from "./trivia/Trivia";
+import { useEffect } from "react";
+import { useMemo } from "react";
 const App = () => {
   const [questionNumber, setquestionNumber] = useState(1);
 
-  const [timeOut, setTimeOut] = useState(false);
+  const [stop, setstop] = useState(false);
+
+  const [earned, setearned] = useState("0");
 
   const data = [
     {
@@ -55,38 +59,53 @@ const App = () => {
     },
   ];
 
-  const moneyPyramid = [
-    { id: 1, amount: "100" },
-    { id: 2, amount: "200" },
-    { id: 3, amount: "300" },
-    { id: 4, amount: "500" },
-    { id: 5, amount: "1000" },
-    { id: 6, amount: "2000" },
-    { id: 7, amount: "4000" },
-    { id: 8, amount: "8000" },
-    { id: 9, amount: "16000" },
-    { id: 10, amount: "32000" },
-    { id: 11, amount: "64000" },
-    { id: 12, amount: "125000" },
-    { id: 13, amount: "250000" },
-    { id: 14, amount: "500000" },
-    { id: 15, amount: "1000000" },
-  ].reverse();
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "100" },
+        { id: 2, amount: "200" },
+        { id: 3, amount: "300" },
+        { id: 4, amount: "500" },
+        { id: 5, amount: "1000" },
+        { id: 6, amount: "2000" },
+        { id: 7, amount: "4000" },
+        { id: 8, amount: "8000" },
+        { id: 9, amount: "16000" },
+        { id: 10, amount: "32000" },
+        { id: 11, amount: "64000" },
+        { id: 12, amount: "125000" },
+        { id: 13, amount: "250000" },
+        { id: 14, amount: "500000" },
+        { id: 15, amount: "1000000" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setearned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [moneyPyramid, questionNumber]);
 
   return (
     <div className="app">
       <div className="main">
-        <div className="top">
-          <div className="timer">30</div>
-        </div>
-        <div className="bottom">
-          <Trivia
-            data={data}
-            setTimeOut={setTimeOut}
-            questionNumber={questionNumber}
-            setquestionNumber={setquestionNumber}
-          />
-        </div>
+        {stop ? (
+          <h1 className="endtext">You Earned: {earned}</h1>
+        ) : (
+          <>
+            <div className="top">
+              <div className="timer">30</div>
+            </div>
+            <div className="bottom">
+              <Trivia
+                data={data}
+                setstop={setstop}
+                questionNumber={questionNumber}
+                setquestionNumber={setquestionNumber}
+              />
+            </div>
+          </>
+        )}
       </div>
 
       <div className="pyramid">
--- src/components/trivia/Trivia.jsx
+++ src/components/trivia/Trivia.jsx
@@ -3,18 +3,49 @@ import { useEffect } from "react";
 import { useState } from "react";
 import "./trivia.css";
 
-const Trivia = ({ data, setTimeOut, questionNumber, setquestionNumber }) => {
+const Trivia = ({ data, setstop, questionNumber, setquestionNumber }) => {
   const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setselectedAnswer] = useState(null);
+  const [className, setclassName] = useState("answer");
+
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
+  const handleClick = (a) => {
+    setselectedAnswer(a);
+    setclassName("answer active");
+    delay(3000, () =>
+      setclassName(a.correct ? "answer correct" : "answer wrong")
+    );
+    delay(6000, () => {
+      if (a.correct) {
+        setquestionNumber((prev) => prev + 1);
+        setselectedAnswer(null);
+      } else {
+        setstop(true);
+      }
+    });
+  };
+
   return (
     <div className="trivia">
       <div className="question">{question?.question}</div>
 
       <div className="answers">
         {question?.answers.map((a) => (
-          <div className="answer">{a.text}</div>
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => handleClick(a)}
+          >
+            {a.text}
+          </div>
         ))}
       </div>
     </div>
